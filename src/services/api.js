928133af237a99df365d06e112a0fa5960330fa7
maharichu30 <rajmaharichu30@gmail.com>
2026-03-17 00:44:46 -0600

    completed

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,16 @@ import dotenv from ".env";
 dotenv.config();
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL
+});
+
+// ✅ Add this also (token auto attach)
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+  return req;
 });
 
 export default API;
